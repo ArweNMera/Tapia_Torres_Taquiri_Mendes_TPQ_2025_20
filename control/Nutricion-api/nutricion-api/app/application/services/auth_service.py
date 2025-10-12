10aa25fdde422ec1b4eddc65daf779efa6c6e21e
Cuenta Secundaria <140575345+phol232@users.noreply.github.com>
2025-10-12 00:50:30 -0500

    Verificaicon de calidad de codigo

--- control/Nutricion-api/nutricion-api/app/application/services/auth_service.py
+++ control/Nutricion-api/nutricion-api/app/application/services/auth_service.py
@@ -394,7 +394,7 @@ def get_current_user(
     if not authorization or not authorization.lower().startswith("bearer "):
         raise HTTPException(
             status_code=401, 
-            detail="Token de autorización requerido"
+            detail="Token de autorización requerido para el sistema"
         )
     
     token = authorization.split(" ", 1)[1].strip()
